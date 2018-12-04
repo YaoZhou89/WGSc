4a92560ee0a6feb6641cc1c9cc9bc3f9c2f7b3bf
GST <zhouyao1989@163.com>
2018-12-04 19:31:50 +0800

    Create HeaderIns.h

---
+++ WGS/HeaderIns.h
@@ -0,0 +1,24 @@
+//
+//  HeaderIns.h
+//  WGS
+//
+//  Created by Yao Zhou on 2018/12/4.
+//  Copyright © 2018 Yao Zhou. All rights reserved.
+//
+
+#ifndef HeaderIns_h
+#define HeaderIns_h
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <iomanip>
+#include <math.h>
+#include <cstdlib>
+#include <zlib.h>
+#include "./include/gzstream/gzstream.c"
+
+#endif /* HeaderIns_h */
