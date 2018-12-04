90b4c79852a24081196f158e01a8264ab7b0890e
GST <zhouyao1989@163.com>
2018-12-04 19:31:42 +0800

    Create checkers.h

---
+++ WGS/checkers.h
@@ -0,0 +1,29 @@
+//
+//  checkers.h
+//  WGS
+//
+//  Created by Yao Zhou on 2018/12/4.
+//  Copyright © 2018 Yao Zhou. All rights reserved.
+//
+
+#ifndef checkers_h
+#define checkers_h
+
+using namespace std;
+
+
+int checkOUT(parameter * para){
+    string outFile = (para -> outFile);
+    outFile = (para -> outFile) + ".gz";
+    ogzstream OUTTest ((outFile).c_str());
+    if((!OUTTest.good()))
+    {
+        cerr << "open OUT File error: "<< outFile <<endl;
+        delete para;
+        return 1;
+    }
+    OUTTest.close();
+    return 0;
+}
+
+#endif /* checkers_h */
