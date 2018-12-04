a022d999e5903dcb0450c1ef88390b30e0b1d100
GST <zhouyao1989@163.com>
2018-12-04 10:03:29 +0800

    Initial Commit

---
+++ WGS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WGS
+//
+//  Created by Yao Zhou on 2018/12/4.
+//  Copyright © 2018 Yao Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
