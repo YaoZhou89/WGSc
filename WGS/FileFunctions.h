2dfce64451c1f13f59a9d7aa728eabedc4be51e5
GST <zhouyao1989@163.com>
2019-01-07 18:47:23 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -810,7 +810,7 @@ int pi(parameter *para){
                 continue;
             }
         }
-        double pi = 2*((a*b)+(b*a))/(sampleSize*(sampleSize-1));
+        double pi = 2*(a+b)/(sampleSize*(sampleSize-1));
         ouf << ll[0] << "\t" << ll[1] << "\t" << pi << endl;
     }
     inF.close();
