3807f8d07f55effe2d4a730c81fe9efe96a11d46
GST <zhouyao1989@163.com>
2019-01-07 17:45:31 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -782,6 +782,7 @@ int pi(parameter *para){
             ll.clear();
             split(line,ll,"\t");
             for(int i = 9; i < ll.size(); ++i){
+                cout << ll[i] << endl;
                 if(Samples.count(ll[i])==1) samplePos.push_back(i);
             }
             continue;
@@ -799,6 +800,8 @@ int pi(parameter *para){
             }
         }
         double pi = 2*(a*b*1.0)/((a+b-1)*1.0);
+        cout << "a is: " << a << endl;
+        cout << "b is: " << b << endl;
         ouf << ll[0] << "\t" << ll[1] << "\t" << pi << endl;
     }
     inF.close();
