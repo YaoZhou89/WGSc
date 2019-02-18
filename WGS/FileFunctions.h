237a44788a208ffa2d968f1377dd958eef3b0a01
GST <zhouyao1989@163.com>
2019-02-18 19:36:08 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -2435,6 +2435,8 @@ int subMummer4(parameter *para){
             continue;
         }
         if(ll[1]=="END"){
+            ss1.clear();
+            ss2.clear();
             for (int i =0; i < s1.length(); ++i){
                 if(s1[i]!='\\.'){
                     ss1 += s1[i];
