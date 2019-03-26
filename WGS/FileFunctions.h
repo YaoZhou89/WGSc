19b3cdac052b9b3e0a0099c62d884c7fd11afc86
GST <zhouyao1989@163.com>
2019-03-26 17:02:08 +0800

    a

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -3294,8 +3294,8 @@ int toXPCLR(parameter *para){
         samples.insert(line);
     }
     cout << "group readed! group size is:\t" << samples.size() << endl;
-    double** value = dmatrix(0,500000000,0,1);
-    double recvalue = 0;
+    long double** value = dmatrix(0,500000000,0,1);
+    long double recvalue = 0;
     int previous = 0;
     while(!inrec.eof()){
         getline(inrec,line);
@@ -3311,6 +3311,7 @@ int toXPCLR(parameter *para){
         int end = string2Int(ll[2]);
         for (int cp = begin; cp < end; ++cp ){
             value[cp][0] = recvalue + bin * (cp-previous);
+            if(value[cp][0]==value[cp-1][0]) value[cp][0] = value[cp][0] + 0.000001;
         }
         previous = end;
         recvalue += string2Double(ll[4]);
