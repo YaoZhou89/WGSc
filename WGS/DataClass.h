946c65e016f324a791c0f1804945d146f3a288b8
GST <zhouyao1989@163.com>
2019-06-11 12:04:29 +0800

    Update DataClass.h

--- WGS/DataClass.h
+++ WGS/DataClass.h
@@ -437,8 +437,9 @@ public:
         
             ll.clear();
 //            cout << line << endl;
-            split(line,ll,"\t");
+            split(line,ll," \t");
             cout << "ll[0] is:\t" << ll[0] << endl;
+            cout << "ll size is:\t" << ll.size() << endl;
             if(ll[0] != chr) continue;
             cout << line << endl;
             if (ll[2] == "gene"){
