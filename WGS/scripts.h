9fc2743355b6ae0fbd36de05f8980e5a99ed0adf
GST <zhouyao1989@163.com>
2020-02-16 01:14:30 +0800

    Update scripts.h

--- WGS/scripts.h
+++ WGS/scripts.h
@@ -18,7 +18,8 @@ int getFastp(parameter *para){
         cerr << "Open file error: " << infile << endl;
         return 0;
     }
-    ofstream ouf ((outfile+"fastp.sh").c_str());
+    string oun = "fastp.sh";
+    ofstream ouf (oun.c_str());
     if(ouf.fail()){
         cerr << "Couldn't open outFile" << endl;
         return 0;
@@ -40,7 +41,7 @@ int getFastp(parameter *para){
     ouf << "#!/bin/bash\n";
     for (it = names.begin(); it != names.end(); it++){
         ouf << "fastp -i " << *it << "_" <<  middle << "1." << suffix << " -I " << *it << "_" <<  middle <<  "2." << suffix <<
-        " --thread " << thread << " -o "<< outfile  << *it << ".R1.fq.gz -O " << *it << ".R2.fq.gz\n";
+        " --thread " << thread << " -o " << outfile  << *it << ".R1.fq.gz -O " << outfile << *it << ".R2.fq.gz\n";
     }
     inf.close();
     ouf.close();
