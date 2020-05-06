67211f334ee27c7a059e3de1b67fc3ab33c1c7d3
GST <zhouyao1989@163.com>
2020-05-06 16:44:02 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -11826,6 +11826,7 @@ int getGenomeMasked (parameter *para){
             seq.append(line);
         }
     }
+    cout << chr <<" length is:\t" << seq.length() << endl;
     for (int i = 0; i < seq.length(); ++i){
         if (seq[i] == 'N') continue;
         ouf << chr << "\t" << i << "\n";
