f3e17d9ff1d7c8b25ecc32cf63b8fe163469563e
GST <zhouyao1989@163.com>
2020-06-16 16:33:44 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -10734,14 +10734,23 @@ int getIBSdistance_bed(parameter *para){
         initialize(maker_tmp);
         initialize(ibd_tmp);
         calibs(pos2,ll,pos1,ibd_tmp,maker_tmp);
-        cout << "C is:\t" << c <<"; marker 8 is:\t" << maker_tmp[8] << endl;
+//        cout << "C is:\t" << c <<"; marker 8 is:\t" << maker_tmp[8] << endl;
         for(int i = 0; i < ibd_tmp.size();i++){
             MARKER_tmp[i] += maker_tmp[i];
             IBD_tmp[i] += ibd_tmp[i];
         }
         cout << "C is:\t" << c <<"; MARKER 8 is:\t" << MARKER_tmp[8] << endl;
-        ibsv.insert(pair<int,vector<double>>(c,IBD_tmp));
-        makerv.insert(pair<int,vector<double>>(c,MARKER_tmp));
+        if (ibsv.count(c) == 0){
+            ibsv.insert(pair<int,vector<double>>(c,IBD_tmp));
+        }else{
+            ibsv[c] = IBD_tmp;
+        }
+        if (makerv.count(c) == 0){
+            makerv.insert(pair<int,vector<double>>(c,MARKER_tmp));
+        }else{
+            makerv[c] = MARKER_tmp;
+        }
+        
     }
     cout << "Passed number is:\t" << passed++ << endl;
     for (int i = 0; i < orders.size(); ++i){
