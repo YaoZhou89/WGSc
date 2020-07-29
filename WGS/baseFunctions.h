272329dda0c71ae10c6803ce4d8751d636f52cf7
GST <zhouyao1989@163.com>
2020-07-29 16:28:27 +0800

    Update baseFunctions.h

--- WGS/baseFunctions.h
+++ WGS/baseFunctions.h
@@ -680,8 +680,10 @@ std::vector<std::string> getSubFoldfiles(std::string path, std::string suffix)
             else if(dirp->d_type == 8)
             {
                 cout << "suffix is:\t" << suffix << endl;
+               
                 if(regex_search(dirp->d_name, reg_obj))
                 {
+                    cout << "name is:\t" << dirp->d_name << endl;
                     string full_path = curr_path + "/" + dirp->d_name;
                     files.push_back(full_path);
                 }
