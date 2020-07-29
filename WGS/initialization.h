d8c5098c73f882730cd42d1e1ce624a3a6535144
GST <zhouyao1989@163.com>
2020-07-29 18:33:58 +0800

    Update initialization.h

--- WGS/initialization.h
+++ WGS/initialization.h
@@ -140,11 +140,11 @@ int WGS_initialization(int argc, char **argv , parameter * para)
             i++;
             para -> minDepth = atof(argv[i]);
         } else if (flag == "pattern")
-               {
-                   if(i + 1 == argc) {LogLackArg(flag);return 0;}
-                   i++;
-                   para -> pattern = atof(argv[i]);
-               }
+        {
+            if(i + 1 == argc) {LogLackArg(flag);return 0;}
+            i++;
+            para -> pattern = argv[i];
+        }
         else if (flag == "maxDepth")
         {
             if(i + 1 == argc) {LogLackArg(flag);return 0;}
