1edabf39935db36d249c78b82304c0822a7817c1
GST <zhouyao1989@163.com>
2020-09-29 13:47:12 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -14368,18 +14368,15 @@ int cigarSimilarity(parameter *para){
     cout << contig_length.size() << " contigs found!" << endl;
     map<string,vector<int>> ar ;
     vector<string> ri ;
-    vector<int> array;
+    
     while (!inf.eof()){
         getline(inf,line);
-        cout << line << endl;
-        cout << "readed!" << endl;
         if(line.length() < 1 ) continue;
         ll.clear();
         split(line,ll," \t");
-        array.clear();
-        array = parseCIGAR(ll[3],contig_length[ll[1]],string2Int(ll[2]));
-        ar.insert(pair<string,vector<int>>(ll[0],array));
-        ri.push_back(ll[0]);
+//        vector<int> array = parseCIGAR(ll[3],contig_length[ll[1]],string2Int(ll[2]));
+//        ar.insert(pair<string,vector<int>>(ll[0],array));
+//        ri.push_back(ll[0]);
     }
     cout <<  ri.size() << " CIGAR values readed!" << endl;
     vector<set<string>> pa;
