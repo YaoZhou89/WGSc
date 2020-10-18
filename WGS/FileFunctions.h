0e609c7799cf55c09837defe90dcb0127dbf3280
GST <zhouyao1989@163.com>
2020-10-18 19:15:35 +0800

    Update FileFunctions.h

--- WGS/FileFunctions.h
+++ WGS/FileFunctions.h
@@ -14653,6 +14653,10 @@ int paf(parameter *para){
                 if(pairsValue.count(idids) == 0){
                     idids = idid[j] + "_" + idid[i];
                 }
+                if(idid[i]=="m64054_191222_151922/48956374/ccs" || idid[j] == "m64054_191222_151922/48956374/ccs"){
+                    ouf << "id1 is:\t" << idid[i] << "; id2 is:\t" << idid[j] << "; value is:\t" << pairsValue[idids];
+                }
+                
                if (pairsValue[idids] < t) kept++;
             }
             if( kept > value.size()*0.5 ){
