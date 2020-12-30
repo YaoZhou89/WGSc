8fd8fe89751b6f940592e5c215fd66bab2d7bc2d
GST <zhouyao1989@163.com>
2020-12-30 22:08:54 +0800

    Update main.cpp

--- WGS/main.cpp
+++ WGS/main.cpp
@@ -194,6 +194,8 @@ int main(int argc, char * argv[]) {
                 splitByChrVCF(para);
             }else if ((para->type) == "SVfilter_reads"){
                 SVfilter_reads(para);
+            }else if ((para->type) == "bed2vcf"){
+                bed2vcf(para);
             }
         }else if((para -> model) == "depth"){
             if((para->type)=="site"){
