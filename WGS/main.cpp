63e52102f64bef446e3d59293547f4812bb712a9
Yao Zhou <zhouyao1989@163.com>
2021-01-02 02:53:33 +0800

    Update main.cpp

--- WGS/main.cpp
+++ WGS/main.cpp
@@ -200,6 +200,8 @@ int main(int argc, char * argv[]) {
                 bed2vcf(para);
             }else if ((para->type) == "mergeSVs"){
                 mergeSVs(para);
+            }else if ((para->type) == "cleanSVs"){
+                cleanSVs(para);
             }
         }else if((para -> model) == "depth"){
             if((para->type)=="site"){
