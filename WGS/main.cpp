bf9a4146e4761f2abcf089f7a21e40a97524aac0
GST <zhouyao1989@163.com>
2021-02-04 21:09:42 +0800

    Update main.cpp

--- WGS/main.cpp
+++ WGS/main.cpp
@@ -264,7 +264,7 @@ int main(int argc, char * argv[]) {
             }else if ((para->type) == "changeBIM"){
                 changeBIM(para);
             }else if ((para->type) == "BIMtype"){
-                siteOverlap(para);
+                BIMtype(para);
             }
         }else if((para->model) == "wc"){
             count(para);
